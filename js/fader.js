d4b0a8a7f57e52b34b7fd6753487dfdb0a6df920
DESKTOP-LCR3IJV\adriu <adrinunezmunoz@gmail.com>
2020-09-24 10:34:31 +0200

    Fixed fader script, allowing Ctrl Click to open a new tab.

---
+++ js/fader.js
@@ -0,0 +1,47 @@
+
+fadeInPage();
+
+function fadeInPage() {
+    if (!window.AnimationEvent) {
+        return;
+    }
+    var fader = document.getElementById('fader');
+    fader.classList.add('fade-out');
+}
+document.addEventListener('DOMContentLoaded', function () {
+    if (!window.AnimationEvent) {
+        return;
+    }
+    var anchors = document.getElementsByTagName('a');
+    for (var idx = 0; idx < anchors.length; idx += 1) {
+        if (anchors[idx].hostname !== window.location.hostname ||
+            anchors[idx].pathname === window.location.pathname) {
+            continue;
+        }
+
+
+        anchors[idx].addEventListener('click', function (event) {
+            if (!event.ctrlKey) {
+                var fader = document.getElementById('fader'),
+                    anchor = event.currentTarget;
+
+                var listener = function () {
+                    window.location = anchor.href;
+                    fader.removeEventListener('animationend', listener);
+                };
+                fader.addEventListener('animationend', listener);
+
+                event.preventDefault();
+                fader.classList.add('fade-in');
+            }
+        });
+    }
+});
+window.addEventListener('pageshow', function (event) {
+    if (!event.persisted) {
+        return;
+    }
+    var fader = document.getElementById('fader');
+    fader.removeAttribute('class', 'fade-in');
+    fader.setAttribute('class', 'fade-out');
+});
