741f6ec0ab1aa8fab1f125a85bfb77ec0d5a4792
JaviCepa <javi@pixelatto.com>
2020-10-21 13:20:02 +0200

    New users

--- functions/timeTrackLogin.js
+++ functions/timeTrackLogin.js
@@ -3,9 +3,12 @@ exports.handler = function(event, context, callback)
     var statusCode = 200; //Ok
     var response = "";
     if (
-        event.headers["user"] == "5e8756796a972807781bb1b2" && event.headers["pass"] == "ozeotropo" ||
-        event.headers["user"] == "5e85aeb96a97283dfc3309aa" && event.headers["pass"] == "amongus" ||
-        event.headers["user"] == "5e821b9d6a9728636371961e" && event.headers["pass"] == "mercadona" ||
+        event.headers["user"] == "5e8756796a972807781bb1b2" || event.headers["user"] == "javi" && event.headers["pass"] == "ozeotropo" ||
+        event.headers["user"] == "5e85aeb96a97283dfc3309aa" || event.headers["user"] == "fali" && event.headers["pass"] == "amongus" ||
+        event.headers["user"] == "5e821b9d6a9728636371961e" || event.headers["user"] == "alberto" && event.headers["pass"] == "mercadona" ||
+        event.headers["user"] == "5f1ec0b6f589af0f490e2f49" || event.headers["user"] == "adri" && event.headers["pass"] == "1992" ||
+        event.headers["user"] == "5ef5d56a6a97287b8d58ce1f" || event.headers["user"] == "palma" && event.headers["pass"] == "coloreando" ||
+        event.headers["user"] == "5e831d956a9728038e712a3d" || event.headers["user"] == "juan" && event.headers["pass"] == "juengine" ||
         false
         )
     {
