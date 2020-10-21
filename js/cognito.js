2941a1821d245b4042df4e60369394ebb813d1da
FaliRonda <rafael.ronda.garcia@gmail.com>
2020-10-21 12:17:12 +0200

    AWS Cognito + signup, login, logout functions

---
+++ js/cognito.js
@@ -0,0 +1,65 @@
+// INIT
+
+var poolData = {
+    UserPoolId: 'us-west-2_LBbnuB8JF',
+    ClientId: '4l9tb5b4pj9u4ecd06ai50hji1'
+};
+
+var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+
+// FUCTIONS
+
+function AWSSignUp(username, email, password) {
+    var attributeEmail = new AmazonCognitoIdentity.CognitoUserAttribute({ Name: 'email', Value: email });
+
+    userPool.signUp(username, password, [attributeEmail], null, (err, data) => {
+        if (err) console.error(err);
+        console.log(data);
+    });
+}
+
+function AWSLogin(username, password) {
+    var authenticationData = {
+        Username: username,
+        Password: password,
+    };
+
+    var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
+
+    var userData = {
+        Username: username,
+        Pool: userPool
+    };
+
+    var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+
+    cognitoUser.authenticateUser(authenticationDetails, {
+        onSuccess: function (result) {
+            // var accessToken = result.getAccessToken().getJwtToken();
+            /* Use the idToken for Logins Map when Federating User Pools with identity pools or when passing through an Authorization Header to an API Gateway Authorizer */
+            sessionStorage.setItem("pixelatto_session_token", result.accessToken.jwtToken)
+        },
+
+        onFailure: function (err) {
+            alert(err);
+        },
+    });
+
+}
+
+function AWSCheckSession() {
+    const user = userPool.getCurrentUser();
+    if (user) {
+        user.getSession((err, session) => {
+            if (err) console.error(err);
+            console.log(session);
+        });
+    }
+}
+
+function AWSSingOut() {
+    const user = userPool.getCurrentUser();
+    if (user) {
+        user.signOut();
+    }
+}
