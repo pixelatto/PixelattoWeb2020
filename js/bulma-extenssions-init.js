ff49fafc4a41b9cc76ba5d3591bca91b069a4d9a
FaliRonda <rafael.ronda.garcia@gmail.com>
2020-10-28 17:06:55 +0100

    Some files formatted, login code commented, unused js removed and js organization

---
+++ js/bulma-extenssions-init.js
@@ -0,0 +1,19 @@
+bulmaCarousel.attach('#product-carousel', {
+    slidesToScroll: 4,
+    slidesToShow: 4,
+    infinite: true
+});
+
+bulmaCarousel.attach('#presskit-carousel', {
+    slidesToScroll: 3,
+    slidesToShow: 3,
+    infinite: true
+});
+
+bulmaCarousel.attach('#testimonials-carousel', {
+    slidesToScroll: 4,
+    slidesToShow: 4,
+    infinite: true
+});
+
+bulmaCollapsible.attach('.is-collapsible');
--- js/main.js
+++ js/main.js
@@ -1,32 +1,9 @@
-const sendOwlKey = "e673fe7e8bf201f";
-const sendOwlSecret = "c48f162eeeb51126494a";
-
 const loadingScreenHTML = document.getElementById("loading-screen");
-
-bulmaCarousel.attach('#product-carousel', {
-    slidesToScroll: 4,
-    slidesToShow: 4,
-    infinite: true
-});
-
-bulmaCarousel.attach('#presskit-carousel', {
-    slidesToScroll: 3,
-    slidesToShow: 3,
-    infinite: true
-});
-
-bulmaCarousel.attach('#testimonials-carousel', {
-    slidesToScroll: 4,
-    slidesToShow: 4,
-    infinite: true
-});
 
 function submitAndRedirectToThanks() {
     document.getElementById("subscribe-form").submit();
     window.location = "/thanks/";
 }
-
-bulmaCollapsible.attach('.is-collapsible');
 
 function loginUser(e) {
     var username = document.getElementById("nick").value;
@@ -55,5 +32,4 @@ function registerUser(e) {
         AWSSignUp(username, email, password);
          window.location = "/";
     }
-
 }
