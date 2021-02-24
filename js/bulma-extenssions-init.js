1213ad05de2877a9c769e39aaf85a5bf1809b9ee
FaliRonda <rafael.ronda.garcia@gmail.com>
2021-02-24 13:23:22 +0100

    Help us page

--- js/bulma-extenssions-init.js
+++ js/bulma-extenssions-init.js
@@ -16,4 +16,35 @@ bulmaCarousel.attach('#testimonials-carousel', {
     infinite: true
 });
 
+if (navigator.userAgent.match(/Android/i)
+    || navigator.userAgent.match(/webOS/i)
+    || navigator.userAgent.match(/iPhone/i)
+    || navigator.userAgent.match(/iPad/i)
+    || navigator.userAgent.match(/iPod/i)
+    || navigator.userAgent.match(/BlackBerry/i)
+    || navigator.userAgent.match(/Windows Phone/i)) {
+    bulmaCarousel.attach('#fans-carousel', {
+        slidesToScroll: 1,
+        slidesToShow: 1,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 3000
+    });
+
+    bulmaCarousel.attach('#helpus-products-carousel', {
+        slidesToScroll: 1,
+        slidesToShow: 1,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 3000
+    });
+} else {
+    bulmaCarousel.attach('#fans-carousel', {
+        slidesToScroll: 4,
+        slidesToShow: 4,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 3000
+    });
+}
 bulmaCollapsible.attach('.is-collapsible');
